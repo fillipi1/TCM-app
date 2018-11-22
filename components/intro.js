3598220a3382ece614d92c304faf39427793b024
Fillipi <fillipi@Fillipis-MacBook-Pro.local>
2018-11-22 00:20:28 -0800

    playing

--- components/intro.js
+++ components/intro.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { Button } from 'react-native';
+import { Button, AppRegistry, TextInput, StyleSheet, Text, View  } from 'react-native';
 
 const intro = () => {
     return ( 
-        <Button title='come in' onPress ={alert('hey buddy :)')}/>
+        <View>
+            <Button 
+            title='come in'
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+            onPress ={console.log('hey buddy :)')}/>
+            <TextInput placeholder='you know wassup' />
+        </View>
+        
     )
 }
 
