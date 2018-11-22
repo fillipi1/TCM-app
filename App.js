9f9b581f8d0d008c0f68c1b263bdb53aa2487298
Fillipi <fillipi@Fillipis-MacBook-Pro.local>
2018-11-21 23:46:56 -0800

    my first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Intro from './components/intro';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Welcome young Jedi</Text>
+        <Intro/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ components/intro.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'react-native';
+
+const intro = () => {
+    return (
+        <Button title='come in' onPress ={alert('hey buddy :)')}/>
+    )
+}
+
+export default intro;
